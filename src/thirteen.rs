1a7d47e0b298ee886f0cfe40ee019dd6a8193485
Brian Gavin <bgavin1996@gmail.com>
2023-12-14 20:09:14 -0800

    13.1

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -1,23 +1,24 @@
 use crate::Input;
 
 pub fn solve1(input: Input) -> usize {
-    parse_input(input).into_iter().for_each(|p| {
-        dbg!(&p);
-        let row_reflect = reflection(p.rows);
-        let col_reflect = reflection(p.cols);
-        dbg!(row_reflect, col_reflect);
-    });
-    0
+    parse_input(input)
+        .into_iter()
+        .map(|p| {
+            let vert = reflection(p.cols).unwrap_or(0);
+            let hori = reflection(p.rows).unwrap_or(0);
+            vert + hori * 100
+        })
+        .sum()
 }
 pub fn solve2(input: Input) -> usize {
     todo!()
 }
 
 fn reflection(v: Vec<String>) -> Option<usize> {
-    (1..v.len() / 2).find(|i| {
+    (1..v.len()).find(|i| {
         let (left, right) = v.split_at(*i);
         if left.len() <= right.len() {
-            left.iter().zip(right.iter().rev()).all(|(a, b)| a == b)
+            left.iter().rev().zip(right.iter()).all(|(a, b)| a == b)
         } else {
             right.iter().zip(left.iter().rev()).all(|(a, b)| a == b)
         }
