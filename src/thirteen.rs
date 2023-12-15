1a17ea14b82aab23f4d3e8ccd549416351790251
Brian Gavin <bgavin1996@gmail.com>
2023-12-14 20:35:02 -0800

    partial 13.2

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -1,26 +1,54 @@
 use crate::Input;
 
+type Eq = fn((&String, &String)) -> bool;
+
 pub fn solve1(input: Input) -> usize {
     parse_input(input)
         .into_iter()
         .map(|p| {
-            let vert = reflection(p.cols).unwrap_or(0);
-            let hori = reflection(p.rows).unwrap_or(0);
+            let vert = reflection(&p.cols, eq).unwrap_or(0);
+            let hori = reflection(&p.rows, eq).unwrap_or(0);
+            dbg!(vert) + dbg!(hori) * 100
+        })
+        .sum()
+}
+
+fn eq((a, b): (&String, &String)) -> bool {
+    a == b
+}
+
+fn eq_off_by_one((a, b): (&String, &String)) -> bool {
+    let mut diff = 0;
+    a.chars().zip(b.chars()).for_each(|(a, b)| {
+        if a != b {
+            diff += 1;
+        }
+    });
+    diff <= 1
+}
+
+pub fn solve2(input: Input) -> usize {
+    parse_input(input)
+        .into_iter()
+        .map(|ref p| {
+            let vert = reflection(&p.cols, eq_off_by_one)
+                .and_then(|i| reflection(&p.cols, eq).and_then(|j| (i != j).then_some(j)))
+                .unwrap_or(0);
+            let hori = reflection(&p.rows, eq_off_by_one)
+                .and_then(|i| reflection(&p.rows, eq).and_then(|j| (i != j).then_some(j)))
+                .unwrap_or(0);
             vert + hori * 100
         })
         .sum()
 }
-pub fn solve2(input: Input) -> usize {
-    todo!()
-}
 
-fn reflection(v: Vec<String>) -> Option<usize> {
+fn reflection(v: &[String], eq: Eq) -> Option<usize> {
     (1..v.len()).find(|i| {
         let (left, right) = v.split_at(*i);
         if left.len() <= right.len() {
-            left.iter().rev().zip(right.iter()).all(|(a, b)| a == b)
+            left.iter().rev().zip(right.iter()).all(eq)
         } else {
-            right.iter().zip(left.iter().rev()).all(|(a, b)| a == b)
+            right.iter().zip(left.iter().rev()).all(eq)
         }
     })
 }
